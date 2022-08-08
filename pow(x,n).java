cf03b86f0fb3f13549ca1873a6f98b7c1d284d9b
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2022-08-08 15:24:51 -0700

    Create pow(x,n).java

---
+++ pow(x,n).java
@@ -0,0 +1,20 @@
+// TC: O(logn)
+// SC: O(logn)
+
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n == 0) return 1.0;
+        
+        double val = myPow(x, n/2);
+        if(n%2 == 0) {
+            return val*val;
+        } else {
+            if(n > 0) {
+                return val*val*x;
+            } else {
+                return val*val* 1/x;
+            }
+        }
+    }
+}
